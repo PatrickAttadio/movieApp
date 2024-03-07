8979ed1c2307d3568652f7177f0235b7dbb2130a
Patrick <patrick.attadio@gmail.com>
2024-03-07 17:53:46 +0100

    Funzione getTrending

--- js/api/config.js
+++ js/api/config.js
@@ -1,10 +1,12 @@
 export const BASEURL = "https://api.themoviedb.org/3/";
 export const person = "Di Caprio";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const OPTIONS = {
       method: "GET",
       headers: {
             "Content-Type": "application/json",
-            Authorization: "Bearer ",
+            Authorization: `Bearer ${process.env.API_KEY}`,
       },
 };
--- js/index.js
+++ js/index.js
@@ -1,7 +1,40 @@
 import { getTrending, getTrendingMovies, getTrendingTV, getMovieFromActor } from "./api/trendingGetters.js";
 import { person } from "./api/config.js";
 
-getTrending();
-getTrendingMovies();
-getTrendingTV();
-getMovieFromActor(person);
+ /**
+  * Da aggiungere:
+  * id
+  * poster_path
+  * title
+  * vote_average
+  * media_type
+  * release_date
+  */
+const trending = getTrending() .then((data) => {
+      data.results.forEach(movie => {
+            console.log("/");
+            console.log(movie.id);
+            console.log(movie.poster_path);
+            if(movie.media_type == "movie"){
+                  console.log(movie.title);
+            } else if (movie.media_type == "tv") {
+                  console.log(movie.name);
+            }
+            console.log(movie.vote_average);
+            console.log(movie.media_type);
+            if(movie.release_date != null){
+                  console.log(movie.release_date);
+            } else {
+                  console.log(movie.first_air_date+" --> first air date");
+            }
+      });
+});
+const trendingMovies = getTrendingMovies() .then((data) => {
+      // console.log(data);
+});
+const trendingTV = getTrending() .then((data) => {
+      // console.log(data);
+});
+const movieFromActor = getMovieFromActor(person) .then((data) => {
+      // console.log(data);
+});
